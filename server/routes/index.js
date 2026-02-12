ce28af6b3a98a5c9ba121f72b85656317f87fdb8
awezali <awezali2006@gmail.com>
2026-02-12 12:48:38 +0530

    fix routes index import

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,25 @@
-const auth = require('./auth');
-const {
-  registerValidation,
-  loginValidation,
-  createWorkspaceValidation,
-  inviteMemberValidation,
-} = require('./validate');
+const express = require('express');
+const router = express.Router();
 
-module.exports = {
-  auth,
-  registerValidation,
-  loginValidation,
-  createWorkspaceValidation,
-  inviteMemberValidation,
-};
+const authRoutes = require('./authRoutes');
+const workspaceRoutes = require('./workspaceRoutes');
+const projectRoutes = require('./projectRoutes');
+const boardRoutes = require('./boardRoutes');
+const taskRoutes = require('./taskRoutes');
+const commentRoutes = require('./commentRoutes');
+const notificationRoutes = require('./notificationRoutes');
+const chatRoutes = require('./chatRoutes');
+const githubRoutes = require('./githubRoutes');
+
+// Mount all routes
+router.use('/auth', authRoutes);
+router.use('/workspaces', workspaceRoutes);
+router.use('/projects', projectRoutes);
+router.use('/boards', boardRoutes);
+router.use('/tasks', taskRoutes);
+router.use('/comments', commentRoutes);
+router.use('/notifications', notificationRoutes);
+router.use('/chat', chatRoutes);
+router.use('/github', githubRoutes);
+
+module.exports = router;
