b4f02fc21f7342eb13b058337373c97bfafee58d
awezali <awezali2006@gmail.com>
2026-02-12 22:02:44 +0530

    fix controllers exports properly

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 router.use('/auth', require('./authRoutes'));
 router.use('/workspaces', require('./workspaceRoutes'));
